79503f1c7a232d26ab59e1eecdbd81b26328b6f4
Vladimir <timos9vs@gmail.com>
2019-07-05 12:23:36 +0300

    add check meridiem, add shift methods

--- time_service.rb
+++ time_service.rb
@@ -2,6 +2,7 @@
 
 # simple module for work with time
 module TimeService
+  # decimal time view
   class TimeRecord
     attr_accessor :hours, :minutes, :meridiem
     def time(time_string)
@@ -11,36 +12,52 @@ module TimeService
       @meridiem = tmp[3]
     end
 
+    def switching_meridiem
+      @meridiem = @meridiem == 'PM' ? 'AM' : 'PM'
+    end
+
     def to_s
-      @hours.to_s + ':' + @minutes.to_s + ' ' + @meridiem
+      @hours.to_s + ':' + @minutes.to_s.rjust(2, '0') + ' ' + @meridiem
     end
   end
-
+  # sub class for calculate change time
   class TimeSystem < TimeRecord
     def initialize(str = '00:00 AM')
       time(str)
     end
 
-    def add_minutes(str, minutes)
-      time(str)
-      @minutes += minutes
-      if @minutes > 60
-        @hours += @minutes/60
-        @minutes = @minutes%60
-      end
+    def shift_hours(hours)
+      @hours += hours
       if @hours > 12
+        switching_meridiem if ((@hours / 12) % 2).odd?
         @hours = @hours % 12
-        if @meridiem == "AM"
-          @meridiem = "PM"
-        else
-          @meridiem == "AM"
-        end
       end
       to_s
+    end
+
+    def shift_minutes(minutes)
+      @minutes += minutes
+      if @minutes > 60
+        shift_hours(@minutes / 60)
+        @minutes = @minutes % 60
+      end
+      to_s
+    end
+
+    def add_minutes(str, minutes)
+      time(str)
+      shift_minutes(minutes)
+    end
+
+    def add_hours(str, hours)
+      time(str)
+      shift_hours(hours)
     end
   end
 end
 
-my_time = TimeService::TimeSystem.new.add_minutes('9:24 PM', 10)
+my_time = TimeService::TimeSystem.new('12:00 AM')
 
-puts my_time.to_s
+# puts my_time.add_minutes('9:25 AM', 25)
+puts my_time.shift_hours(88)
+puts my_time.shift_minutes(25)
